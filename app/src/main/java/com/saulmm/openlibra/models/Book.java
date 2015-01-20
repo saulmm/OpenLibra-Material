2d3d6ee4edcba272eae134e1a86c7b63be3fff40
Saul M <saulmm2@gmail.com>
2015-01-20 03:39:20 +0100

    Added a parser to well format html entities

--- app/src/main/java/com/saulmm/openlibra/models/Book.java
+++ app/src/main/java/com/saulmm/openlibra/models/Book.java
@@ -1,7 +1,5 @@
 package com.saulmm.openlibra.models;
 
-import android.support.v7.graphics.Palette;
-
 import java.io.Serializable;
 
 public class Book implements Serializable {
@@ -9,7 +7,56 @@ public class Book implements Serializable {
     private String title;
     private String cover;
     private String author;
+    private String content;
+    private String publisher;
+    private String publisher_date;
+    private String pages;
+    private String rating;
 
+    public String getContent() {
+
+        if (content != null) {
+
+            content = replaceHtmlEntities(content);
+        }
+
+        return content;
+    }
+
+    public String replaceHtmlEntities (String content) {
+
+        return content.replace("&Atilde&iexcl","á")
+            .replace("&Atilde&copy","e")
+            .replace("copy;","e")
+            .replace("&Atilde&shy","i")
+            .replace(";shy","i")
+            .replace("&Atilde&sup3","ó")
+            .replace("sup3;","ó")
+            .replace("sup3;","ó")
+            .replace("&Atilde;&plusmn","ñ")
+            .replace("&Atilde;&","")
+            .replace("&lt;ul&gt;", "")
+            .replace("&lt;/li&gt;", "")
+            .replace("&lt;/ul&gt;", "")
+            .replace("&lt;li&gt;", "\n\t\t");
+
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getPublisher_date() {
+        return publisher_date;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public String getRating() {
+        return rating;
+    }
 
     public String getTitle() {
         return title;
