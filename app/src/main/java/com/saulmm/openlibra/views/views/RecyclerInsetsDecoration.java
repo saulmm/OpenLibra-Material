18656bd27c11897ecf5d63114a66d340c3b0d1c8
Saul M <saulmm2@gmail.com>
2015-01-20 02:50:21 +0100

    Added a Itemdecoration that around each view of the recycler view

---
+++ app/src/main/java/com/saulmm/openlibra/views/views/RecyclerInsetsDecoration.java
@@ -0,0 +1,32 @@
+package com.saulmm.openlibra.views.views;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.saulmm.openlibra.R;
+
+
+/**
+ * ItemDecoration implementation that applies an inset margin
+ * around each child of the RecyclerView. The inset value is controlled
+ * by a dimension resource.
+ *
+ * by Dave Smith at: https://github.com/devunwired/recyclerview-playground
+ */
+public class RecyclerInsetsDecoration extends RecyclerView.ItemDecoration {
+
+    private int mInsets;
+
+    public RecyclerInsetsDecoration(Context context) {
+        mInsets = context.getResources().getDimensionPixelSize(R.dimen.card_insets);
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+
+        //We can supply forced insets for each item view here in the Rect
+        super.getItemOffsets(outRect, view, parent, state);
+    }
+}
