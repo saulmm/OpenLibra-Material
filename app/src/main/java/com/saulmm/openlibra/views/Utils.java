4862e67b1c93f84f916aa8a8cc472ed801033275
Saul M <saulmm2@gmail.com>
2015-01-21 00:38:48 +0100

    Documented Utils class

--- app/src/main/java/com/saulmm/openlibra/views/Utils.java
+++ app/src/main/java/com/saulmm/openlibra/views/Utils.java
@@ -12,6 +12,13 @@ public class Utils {
     public final static int COLOR_ANIMATION_DURATION = 1000;
     public final static int DEFAULT_DELAY = 0;
 
+    /**
+     * Change the color of a view with an animation
+     *
+     * @param v the view to change the color
+     * @param startColor the color to start animation
+     * @param endColor the color to end the animation
+     */
     public static void animateViewColor (View v, int startColor, int endColor) {
 
         ObjectAnimator animator = ObjectAnimator.ofObject(v, "backgroundColor",
@@ -22,28 +29,63 @@ public class Utils {
         animator.start();
     }
 
+    /**
+     * Scale and set the pivot when the animation will start from
+     *
+     * @param v the view to set the pivot
+     */
     public static void configuredHideYView (View v) {
 
         v.setScaleY(0);
         v.setPivotY(0);
     }
 
+    /**
+     * Reduces the X & Y from a view
+     *
+     * @param v the view to be scaled
+     *
+     * @return the ViewPropertyAnimation to manage the animation
+     */
     public static ViewPropertyAnimator hideViewByScaleXY(View v) {
 
         return hideViewByScale(v, DEFAULT_DELAY, 0, 0);
     }
 
+    /**
+     * Reduces the Y from a view
+     *
+     * @param v the view to be scaled
+     *
+     * @return the ViewPropertyAnimation to manage the animation
+     */
     public static ViewPropertyAnimator hideViewByScaleY(View v) {
 
         return hideViewByScale(v, DEFAULT_DELAY, 1, 0);
     }
 
-
+    /**
+     * Reduces the X from a view
+     *
+     * @param v the view to be scaled
+     *
+     * @return the ViewPropertyAnimation to manage the animation
+     */
     public static ViewPropertyAnimator hideViewByScalyInX(View v) {
 
         return hideViewByScale(v, DEFAULT_DELAY, 0, 1);
     }
 
+    /**
+     * Reduces the X & Y
+     *
+     * @param v the view to be scaled
+     * @param delay to start the animation
+     * @param x integer to scale
+     * @param y integer to scale
+     *
+     * @return the ViewPropertyAnimation to manage the animation
+     */
     private static ViewPropertyAnimator hideViewByScale (View v, int delay, int x, int y) {
 
         ViewPropertyAnimator propertyAnimator = v.animate().setStartDelay(delay)
@@ -52,6 +94,13 @@ public class Utils {
         return propertyAnimator;
     }
 
+    /**
+     * Shows a view by scaling
+     *
+     * @param v the view to be scaled
+     *
+     * @return the ViewPropertyAnimation to manage the animation
+     */
     public static ViewPropertyAnimator showViewByScale (View v) {
 
         ViewPropertyAnimator propertyAnimator = v.animate().setStartDelay(DEFAULT_DELAY)
